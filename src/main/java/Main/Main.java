02d6c1a1b070d9965e4660e2d0500618f96e9c94
Felipe Ramirez <felipe.ramirezrivera@gmail.com>
2022-04-22 01:05:34 -0500

    Created the Serialization function and the question Database

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,30 +1,43 @@
 package Main;
 
+import DataBase.Seriallization;
 import Game.Player;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player("Juan", 0);
+        Seriallization saveData = new Seriallization();
 
-        //System.out.println(player.getName());
-        HashMap borrar = new HashMap();
-        ArrayList borrar2 = new ArrayList();
-        borrar2.add(15);
-        borrar2.add(15);
-        borrar2.add(15);
-        borrar2.add(15);
-
-        //System.out.println(borrar2);
-
-        borrar.put("option",borrar2);
-
-        Object[] opciones = borrar.values().toArray();
-        for (Object o : borrar.values().toArray()) {
-            System.out.println(o.getClass());
+        try {
+            saveData.savingData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+
+
+//        Player player = new Player("Juan", 0);
+//
+//        //System.out.println(player.getName());
+//        HashMap borrar = new HashMap();
+//        ArrayList borrar2 = new ArrayList();
+//        borrar2.add(15);
+//        borrar2.add(15);
+//        borrar2.add(15);
+//        borrar2.add(15);
+//
+//        //System.out.println(borrar2);
+//
+//        borrar.put("option",borrar2);
+//
+//        Object[] opciones = borrar.values().toArray();
+//        for (Object o : borrar.values().toArray()) {
+//            System.out.println(o.getClass());
+//        }
 
         //System.out.println();
 
